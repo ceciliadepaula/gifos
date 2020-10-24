df63c190a480513f79654d124570b4a18e5e2705
Cecilia De Paula <depaulacecilia@gmail.com>
2020-10-24 20:12:01 -0300

    Diurno Nocturno

--- scripts/nocturno.js
+++ scripts/nocturno.js
@@ -1,12 +1,40 @@
 //////  ------ NOCTURNO ------  //////
 
-    // agregar lo de local storage
-    //y cambiar palabra nocturno*duirno
-
-let botonNoct = document.getElementById("nocturno");
+/* let botonNoct = document.getElementById("nocturno");
 botonNoct.addEventListener("click", Nocturno);
 
 function Nocturno(){
     var body = document.getElementsByTagName("body");
     var guardado = body[0].classList.toggle("nocturno");
-} 
+}  */
+
+let darkMode = localStorage.getItem("darkMode");
+let botonNoct = document.getElementById("nocturno");
+var body = document.getElementsByTagName("body");
+botonNoct.addEventListener("click", Nocturno);
+let modoDisenio = document.getElementById("modoDisenio");
+
+const Noche = () => {
+    body[0].classList.add("nocturno");
+    localStorage.setItem("darkMode", "enabled");    
+    modoDisenio.innerHTML = "Modo diurno";
+}
+
+const Dia = () => {
+    body[0].classList.remove("nocturno");
+    localStorage.setItem("darkMode", null);
+    modoDisenio.innerHTML = "Modo nocturno";
+}
+
+if (darkMode === "enabled"){
+    Noche();
+}
+
+function Nocturno() {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        Noche();
+    } else {
+        Dia();
+    }
+}
