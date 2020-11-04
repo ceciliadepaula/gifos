abb935b73d1d3f1d697060f1713d509b3230c196
Cecilia De Paula <depaulacecilia@gmail.com>
2020-11-04 14:30:02 -0300

    Imagenes nuevas y cambios trendings con async

--- scripts/tarjetaBusqueda.js
+++ scripts/tarjetaBusqueda.js
@@ -1,6 +1,73 @@
 let tarjetaBusqueda = document.getElementById("tarjetaBusqueda");
+/* let listaSugerencias = document.createElement("ul"); */
+
+let listaSugerencias = document.getElementById("listaSugerencias");
+
+
+tarjetaBusqueda.addEventListener("input", Tipeando);
+
+function Tipeando() {
+    var link = "https://api.giphy.com/v1/gifs/search/tags?api_key=qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC&q=" + tarjetaBusqueda.value + "&limit=20&offset=5&rating=g&lang=en";
+    Autocompletar(link);
+    MostrarSugerencias();
+} 
+
+function Autocompletar(linkPalabrasAutocompletadas) {
+    fetch(linkPalabrasAutocompletadas)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(respuestaTransformada => {
+            for (i = 0; i < 4; i++) {
+                let listadoAutocompletado = document.createElement("li");
+
+                /* listadoAutocompletado.style.color = "#9CAFC3"; */
+
+                listadoAutocompletado.innerHTML = respuestaTransformada.data[i].name;
+                listaSugerencias.appendChild(listadoAutocompletado); 
+                
+            }
+
+            /* console.log(listaSugerencias); */
+
+        })
+        .catch(error => {
+            console.log("Error: " + error);
+        });
+} 
+
+
+
+function MostrarSugerencias(){   // cambio de estilo
+    
+
+}
 
 
 
 
+/*     let completarCampo2 = document.getElementById("completar2");
+       let listaSugerida2 = document.getElementById("listaSugerida2");
+       completarCampo2.addEventListener("keydown", Tipeando);
 
+       function Tipeando() {
+           var link = "https://api.giphy.com/v1/gifs/search/tags?api_key=qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC&q=" + completarCampo2.value + "&limit=20&offset=5&rating=g&lang=en";
+           Autocompletar(link);
+       }
+
+       function Autocompletar(linkPalabrasAutocompletadas) {
+           fetch(linkPalabrasAutocompletadas)
+               .then(respuesta => {
+                   return respuesta.json();
+               })
+               .then(respuestaTransformada => {
+                   for (i = 0; i < 4; i++) {
+                       let listadoAutocompletado = document.createElement("li");
+                       listadoAutocompletado.innerHTML = respuestaTransformada.data[i].name;
+                       listaSugerida2.appendChild(listadoAutocompletado);
+                   }
+               })
+               .catch(error => {
+                   console.log("Errorrr: " + error);
+               });
+       } */
--- scripts/trendings.js
+++ scripts/trendings.js
@@ -6,28 +6,63 @@
     return word[0].toUpperCase() + word.slice(1);
   } */
 
-  var arrayTrendings = [];
+const apikey = "qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC";
 
-  function trendings(url) {
-      let resultadosTrendings = document.getElementById("resultadosTrendings");
-      
-      fetch(url)
-          .then(respuesta => {
-              return respuesta.json();
-          })
-          .then(listaJson => {
-              for (i = 0; i < 5; i++) {
-                  arrayTrendings.push(listaJson.data[i]);
-              }
-              array_aString = new String (arrayTrendings);
-              for (i = 0; i < 4 ; i++ ) {
-                  array_aString =  array_aString.replace(",", " - " );
-              }
-              resultadosTrendings.innerHTML= array_aString;
-          })
-          .catch(error => {
-              console.log("Error!!!: " + error);
-          })
-  }
-  
-  trendings("https://api.giphy.com/v1/trending/searches?api_key=qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC")
+var arrayTrendings = [];
+
+/// VERSIÓN CORTA CON ASYNC
+
+async function trendings(url) {
+
+    let resultadosTrendings = document.getElementById("resultadosTrendings");
+
+    try {
+        let respuesta = await fetch(url);
+        let listaJson = await respuesta.json();
+
+        for (i = 0; i < 5; i++) {
+            arrayTrendings.push(listaJson.data[i]);
+        }
+        array_aString = new String(arrayTrendings);
+        for (i = 0; i < 4; i++) {
+            array_aString = array_aString.replace(",", " - ");
+        }
+        resultadosTrendings.innerHTML = array_aString;
+    }
+    catch (error) {
+        console.log("Error!!!: " + error);
+    }
+}
+
+trendings(`https://api.giphy.com/v1/trending/searches?api_key=${apikey}`)
+
+
+
+
+/// VERSIÓN LARGA CON .THEN
+
+/* function trendings(url) {
+    let resultadosTrendings = document.getElementById("resultadosTrendings");
+    
+    fetch(url)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(listaJson => {
+            for (i = 0; i < 5; i++) {
+                arrayTrendings.push(listaJson.data[i]);
+            }
+            array_aString = new String (arrayTrendings);
+            for (i = 0; i < 4 ; i++ ) {
+                array_aString =  array_aString.replace(",", " - " );
+            }
+            resultadosTrendings.innerHTML= array_aString;
+        })
+        .catch(error => {
+            console.log("Error!!!: " + error);
+        })
+} 
+ 
+ trendings(`https://api.giphy.com/v1/trending/searches?api_key=${apikey}`)
+ 
+*/
