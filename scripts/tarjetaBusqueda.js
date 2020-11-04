77b3d6d8900068275d07a4978705fdac08deee1c
Cecilia De Paula <depaulacecilia@gmail.com>
2020-11-04 18:20:29 -0300

    Cuadro de busqueda

--- scripts/tarjetaBusqueda.js
+++ scripts/tarjetaBusqueda.js
@@ -1,73 +1,72 @@
 let tarjetaBusqueda = document.getElementById("tarjetaBusqueda");
-/* let listaSugerencias = document.createElement("ul"); */
-
 let listaSugerencias = document.getElementById("listaSugerencias");
+let lupaAzul = document.getElementById("lupaAzul")
+let liLupa = document.getElementsByClassName("liLupa");
+let li1 = document.getElementById("li1");
+let li2 = document.getElementById("li2");
+let li3 = document.getElementById("li3");
+let li4 = document.getElementById("li4");
 
+arrayBusqueda = ["", "", "", ""];
 
 tarjetaBusqueda.addEventListener("input", Tipeando);
 
 function Tipeando() {
-    var link = "https://api.giphy.com/v1/gifs/search/tags?api_key=qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC&q=" + tarjetaBusqueda.value + "&limit=20&offset=5&rating=g&lang=en";
+    var link = `https://api.giphy.com/v1/gifs/search/tags?api_key=${apikey}&q=${tarjetaBusqueda.value}&limit=20&offset=5&rating=g&lang=en`;
     Autocompletar(link);
-    MostrarSugerencias();
-} 
+}
 
 function Autocompletar(linkPalabrasAutocompletadas) {
     fetch(linkPalabrasAutocompletadas)
         .then(respuesta => {
+            //Cambio estilo íconos
+            document.getElementById("lupaGris").style.opacity = "1";
+            document.getElementById("lineaGris").style.opacity = "1";
+            lupaAzul.src = "images/close.svg";
+            lupaAzul.style.height = "15px";
+            tarjetaBusqueda.style.color = "#000000";
             return respuesta.json();
         })
         .then(respuestaTransformada => {
             for (i = 0; i < 4; i++) {
-                let listadoAutocompletado = document.createElement("li");
-
-                /* listadoAutocompletado.style.color = "#9CAFC3"; */
-
-                listadoAutocompletado.innerHTML = respuestaTransformada.data[i].name;
-                listaSugerencias.appendChild(listadoAutocompletado); 
-                
+                arrayBusqueda.unshift(respuestaTransformada.data[i].name);
+                arrayBusqueda.pop();
             }
+            li1.innerHTML = arrayBusqueda[0];
+            li2.innerHTML = arrayBusqueda[1];
+            li3.innerHTML = arrayBusqueda[2];
+            li4.innerHTML = arrayBusqueda[3];
 
-            /* console.log(listaSugerencias); */
+            li1.style.display = "inherit";
+            li2.style.display = "inherit";
+            li3.style.display = "inherit";
+            li4.style.display = "inherit";
 
+            /////////// VER PORQUE NO ME MUYESTRA LAS LUPAS
+            
+            liLupa[0].style.listStyleImage = " url('../images/icon-search-gris-resultado.svg')"
         })
         .catch(error => {
             console.log("Error: " + error);
         });
-} 
+}
 
+///////////////////////////////////////////////////
 
+lupaAzul.addEventListener("click", SacarInfo);
 
-function MostrarSugerencias(){   // cambio de estilo
-    
-
+function SacarInfo() {
+    li1.style.display = "none";
+    li2.style.display = "none";
+    li3.style.display = "none";
+    li4.style.display = "none";
+    document.getElementById("lineaGris").style.opacity = "0";
+    document.getElementById("lupaGris").style.opacity = "0";
+    lupaAzul.src = "images/icon-search.svg";
+    lupaAzul.style.height= "20px";
+    tarjetaBusqueda.value= "";
 }
 
 
+////////////////////////////////////////////////////
 
-
-/*     let completarCampo2 = document.getElementById("completar2");
-       let listaSugerida2 = document.getElementById("listaSugerida2");
-       completarCampo2.addEventListener("keydown", Tipeando);
-
-       function Tipeando() {
-           var link = "https://api.giphy.com/v1/gifs/search/tags?api_key=qh1YngMeTeNY1hh1ul31qyOxXD9gAjLC&q=" + completarCampo2.value + "&limit=20&offset=5&rating=g&lang=en";
-           Autocompletar(link);
-       }
-
-       function Autocompletar(linkPalabrasAutocompletadas) {
-           fetch(linkPalabrasAutocompletadas)
-               .then(respuesta => {
-                   return respuesta.json();
-               })
-               .then(respuestaTransformada => {
-                   for (i = 0; i < 4; i++) {
-                       let listadoAutocompletado = document.createElement("li");
-                       listadoAutocompletado.innerHTML = respuestaTransformada.data[i].name;
-                       listaSugerida2.appendChild(listadoAutocompletado);
-                   }
-               })
-               .catch(error => {
-                   console.log("Errorrr: " + error);
-               });
-       } */
