d4229f1f71d7406d53e5f2139ace7bfbe38bcee1
Cecilia De Paula <depaulacecilia@gmail.com>
2020-11-18 19:24:05 -0300

    Ampliar imagen

--- scripts/tarjetaBusqueda.js
+++ scripts/tarjetaBusqueda.js
@@ -16,20 +16,22 @@ let botonVerMas = document.getElementById("botonVerMas");
 let formBusqueda = document.getElementById("form");
 let ouchResultados = document.getElementById("ouchResultados");
 let gifTrending = document.getElementById("gifTrending");
+let iconoAgrandar = document.getElementsByClassName("iconoAgrandar");
+let agrandarGif = document.getElementById("agrandarGif");
 
 
 // Barra busqueda sticky en top bar
 
-window.onscroll = function() {PosicionBarraBusqueda()};
+window.onscroll = function () { PosicionBarraBusqueda() };
 var sticky = formBusqueda.offsetTop;
 
 function PosicionBarraBusqueda() {
-  if (window.pageYOffset >= 500) {
-    formBusqueda.classList.add("sticky");   // FALTA PONER QUE CUANDO LA BUSQUEDA ESTE ARRIBA, NO SE MUESTREN LOS RESULTADOS DESPLEGADOS
-                                            // Y PONER LINES DE SOMBRA
-  } else {
-    formBusqueda.classList.remove("sticky");    // PERO SI VUELVE ACÁ, SI SE MUESTREN
-  }
+    if (window.pageYOffset >= 500) {
+        formBusqueda.classList.add("sticky");   // FALTA PONER QUE CUANDO LA BUSQUEDA ESTE ARRIBA, NO SE MUESTREN LOS RESULTADOS DESPLEGADOS
+        // Y PONER LINES DE SOMBRA
+    } else {
+        formBusqueda.classList.remove("sticky");    // PERO SI VUELVE ACÁ, SI SE MUESTREN
+    }
 }
 
 // Funciones de Búsqueda
@@ -57,19 +59,19 @@ function Autocompletar(linkPalabrasAutocompletadas) {
 
             /* console.log(respuestaTransformada.data) */
 
-            if (respuestaTransformada.data == 0){
-                
+            if (respuestaTransformada.data == 0) {
+
                 //PONER MENSAJE DE ERROR!!!!!!!!!!!!! Y BORRAR 4 SUGERENCIAS
                 //ver que no es respyestatransformada.DATA... el mensaje tiene que aparecer cuando la palabra no tiene ningun gif
                 ouchResultados.style.display = "flex";
                 botonVerMas.style.display = "none";
-                
-                
+
+
                 /* listaSugerencias.style.display = "none";
                 grillaBusquedaPersonal.style.display = "none";
                 
                  */
-                
+
             } else {
                 ouchResultados.style.display = "none";
                 botonVerMas.style.display = "inherit";
@@ -140,37 +142,40 @@ function MostrarResultadosBusquedaPersonal() {
 }
 
 
-function TraerResultadosBusqueda(limiteMostrar, posicion){
-    
+function TraerResultadosBusqueda(limiteMostrar, posicion) {
+
     url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${palabraBuscada}&limit=${limiteMostrar}&offset=${posicion}`;
 
     fetch(url)
         .then(respuesta => {
             return respuesta.json();
         })
-        .then( nuevoObjetoRecibido => {
+        .then(nuevoObjetoRecibido => {
             for (i = 0; i < 12; i++) {
-                console.log(nuevoObjetoRecibido);
-
                 let contenedorImg = document.createElement("div");
                 contenedorImg.classList.add("GifTrending");
                 contenedorImg.innerHTML = `
-                <img src=${nuevoObjetoRecibido.data[i].images.original.url}>
-                <div class='pasarMouse'> <div class='iconos'>" 
-                <img class='iconoCorazon'src='images/icon-fav.svg' alt='Ícono añadir a favoritos'>
-                <a onclick="DescargarUnGif('${nuevoObjetoRecibido.data[i].id}')">        
-                    <img class='iconoDescargar' src='images/icon-download.svg' alt='Ícono download'>
-                </a>
-                <img class='iconoAgrandar' src='images/icon-max-normal.svg' alt='Ícono maximizar'>
-                </div><div class='infoTexto'><p>
-                ${nuevoObjetoRecibido.data[i].username}</p><h4>
-                ${nuevoObjetoRecibido.data[i].title}</h4></div></div>
-            `
+                    <img src=${nuevoObjetoRecibido.data[i].images.original.url}>
+                    <div class='pasarMouse'> 
+                        <div class='iconos'>" 
+                            <img class='iconoCorazon' id="${nuevoObjetoRecibido.data[i].id}" src='images/icon-fav.svg' alt='Ícono añadir a favoritos'>
+                            <a onclick="DescargarUnGif('${nuevoObjetoRecibido.data[i].id}')">        
+                                <img class='iconoDescargar' src='images/icon-download.svg' alt='Ícono download'>
+                            </a>
+                            <a onclick="AgrandarGif('${nuevoObjetoRecibido.data[i].id}')"> 
+                                <img class='iconoAgrandar' src='images/icon-max-normal.svg' alt='Ícono maximizar'>
+                            </a>
+                        </div>
+                        <div class='infoTexto'>
+                            <p>${nuevoObjetoRecibido.data[i].username}</p>
+                            <h4>${nuevoObjetoRecibido.data[i].title}</h4>
+                        </div>
+                    </div>
+                `
                 grillaBusquedaPersonal.appendChild(contenedorImg);
             }
-            /* barraTrending[0].style.display = "none";        */ 
         })
-        .catch (error => {
+        .catch(error => {
             console.log("Error! " + error);
         });
 }
@@ -178,11 +183,11 @@ function TraerResultadosBusqueda(limiteMostrar, posicion){
 // Sumar 12 gifs cada vez que se haga click en botón Ver Más
 
 var limiteMostrar = 12;
-var posicion = 0; 
+var posicion = 0;
 
-botonVerMas.addEventListener("click", VerMasResultados );
+botonVerMas.addEventListener("click", VerMasResultados);
 
-function VerMasResultados(){
+function VerMasResultados() {
     limiteMostrar = limiteMostrar + 12;
     posicion = posicion + 12;
     TraerResultadosBusqueda(limiteMostrar, posicion);
@@ -193,8 +198,41 @@ function VerMasResultados(){
 
 async function DescargarUnGif(gifoImg) {
     let blob = await fetch(
-      "https://media.giphy.com/media/" + gifoImg + "/giphy.gif"
+        "https://media.giphy.com/media/" + gifoImg + "/giphy.gif"
     ).then((img) => img.blob());
-        console.log(blob);
+    console.log(blob);
     invokeSaveAsDialog(blob, "");
-  }
+}
+
+
+// Maximizar gif
+
+function AgrandarGif(gifoImg) {
+    url = `https://api.giphy.com/v1/gifs/${gifoImg}?api_key=${apikey}`;
+
+    fetch(url)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(nuevoObjeto => {
+            agrandarGif.style.display = "flex";
+            agrandarGif.innerHTML = `
+            <img id="iconoCerrar" src="images/close.svg" alt="Ícono Cerrar ventana">
+            <div id="agregarElGifAqui">
+                <img src=${nuevoObjeto.data.images.original.url}>
+            </div>
+            <div id="datosAmpliado">
+                <div id="datosGifAmpliado">
+                    <p>${nuevoObjeto.data.username}</p>
+                    <h4>${nuevoObjeto.data.title}</h4>
+                </div>
+            <div id="botones">
+                <img src="images/icon-download.svg" alt="Ícono descarga">
+                <img src="images/icon-fav.svg" alt="Ícono corazón">
+            </div>
+        `
+        })
+        .catch(error => {
+            console.log("Error: " + error);
+        })
+}
