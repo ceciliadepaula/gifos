a8d35a30fc83550d8bae9fd769253d2027c716fe
Cecilia De Paula <depaulacecilia@gmail.com>
2020-11-22 16:04:54 -0300

    Crear gifos

--- scripts/crearGif.js
+++ scripts/crearGif.js
@@ -1,15 +1,29 @@
 // Traigo elementos del html a usar o cambiar
-uno = document.getElementById("uno");
-dos = document.getElementById("dos");
-tres = document.getElementById("tres");
-botonComenzar = document.getElementById("botonComenzar");
-tituloPermisos = document.getElementById("tituloPermisos");
-textoPermisos = document.getElementById("textoPermisos");
+let uno = document.getElementById("uno");
+let dos = document.getElementById("dos");
+let tres = document.getElementById("tres");
 
-video = document.getElementById("video");
+let botonComenzar = document.getElementById("botonComenzar");
+let botonGrabar = document.getElementById("botonGrabar");
+let botonFinalizar = document.getElementById("botonFinalizar");
+let botonSubir = document.getElementById("botonSubir");
+
+let tituloPermisos = document.getElementById("tituloPermisos");
+let textoPermisos = document.getElementById("textoPermisos");
+let gifGrabado = document.getElementById("gifGrabado");
+
+let video = document.getElementById("video");
 video.style.display = "none";
 
-// Defino Eventos
+let recorder;
+let blob;
+let form = new FormData();
+
+/* let dateStarted; */
+
+
+
+// Pedir permisos para acceder
 botonComenzar.addEventListener("click", PantallaPermisos);
 
 function PantallaPermisos() {
@@ -17,11 +31,12 @@ function PantallaPermisos() {
     textoPermisos.innerHTML = "El acceso a tu camara será válido sólo <br> por el tiempo en el que estés creando el GIFO.";
     uno.style.backgroundColor = "#572EE5";
     uno.style.color = "#ffffff";
-    botonComenzar.style.visibility = "hidden";
-    dos.addEventListener("click", AbrirCamara);  // Hacer que cuando paso el mouse por arriba se muestre la mano  a:hover { cursor: pointer; } 
+    botonComenzar.style.display = "none";
+    dos.addEventListener("click", AbrirCamara);
 }
 
-// Dar acceso a la camara y mostrar en tiempo real
+
+// Abrir la cámara y mostrar en tiempo real
 
 async function AbrirCamara() {
     dos.style.backgroundColor = "#572EE5";
@@ -30,14 +45,13 @@ async function AbrirCamara() {
     textoPermisos.style.display = "none";
     uno.style.backgroundColor = "#ffffff";
     uno.style.color = "#572EE5";
-    botonComenzar.style.visibility = "visible";
-    botonComenzar.innerHTML = "<p> Grabar </p>";
+    botonGrabar.style.display = "inherit";
     video.style.display = "inherit";
 
     resultado = await getStreamAndRecord();
-    video.src = resultado;
+    /* video.src = resultado; */
 
-    botonComenzar.addEventListener("click", GrabarVideo);
+    botonGrabar.addEventListener("click", GrabarVideo);
 }
 
 function getStreamAndRecord() {
@@ -49,73 +63,58 @@ function getStreamAndRecord() {
     })
         .then(function (stream) {
             video.srcObject = stream;
-            video.play();
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            recorder = RecordRTC(stream, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 430,
+                hidden: 240,
+                onGifRecordingStarted: function () {
+                    console.log('started')
+                },
+            });
         })
         .catch(err => {
             console.log("Error: " + err);
         });
 }
 
-// Empezar a grabar video
-
-/* let recorder = RecordRTC(stream, {
-    type: 'gif',
-    frameRate: 1,
-    quality: 10,
-    width: 360,
-    hidden: 240,
-    onGifRecordingStarted: function () {
-        console.log('started')
-    },
-}); */
+// Grabar video
 
 function GrabarVideo() {
-
     uno.style.backgroundColor = "#ffffff";
     uno.style.color = "#572EE5";
+    botonGrabar.style.display = "none";
+    botonFinalizar.style.display = "inherit";
 
-    botonComenzar.style.visibility = "visible";
-    botonComenzar.innerHTML = "<p> Finalizar </p>";
-    botonComenzar.addEventListener("click", FinalizarGrabacion);
+    recorder.startRecording();
+    botonFinalizar.addEventListener("click", FinalizarGrabacion);
 
-    // Agregar conteo a la derecha
-
-    /*     let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-    let recorder = new RecordRTCPromisesHandler(stream, {
-        type: 'video'
-    });
-    recorder.startRecording(); */
-
-
-    /* recorder.startRecording(); */
-
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-    }).then(async function (stream) {
-        /* let recorder = RecordRTC(stream, {
-            type: 'gif'
-        }); */
-        recorder.startRecording();
-
-    });
 }
 
-// Parar filmación
+// Finalizar grabación
 
 function FinalizarGrabacion() {
 
-    //-----------------------
+    //me tiene que aparecer el texto de si quiere repetirlo.  si lo clickea, va a granar video de nuevpo
+
+    botonFinalizar.style.display = "none";
+    botonSubir.style.display = "inherit";
 
     recorder.stopRecording(function () {
-        let blob = recorder.getBlob();
-        invokeSaveAsDialog(blob);
-
-        alert("termino");
+        video.style.display = "none";
+        gifGrabado.style.display = "inherit";
+        blob = recorder.getBlob();
+        // invokeSaveAsDialog(blob); // me lo guarda en la compu
+        gifGrabado.src = URL.createObjectURL(recorder.getBlob());
+        form.append("file", recorder.getBlob(), "myGif.gif");
+        form.append("api_key", apikey);
     });
 
-    //boton subir gifos. hacer evento al hacer click SubirGrabacionAGifos
-    //donde aparecia el conteo, poner repetir captiura
+    botonSubir.addEventListener("click", SubirGrabacionAGifos);
 }
 
 // Subir a Gifos
@@ -123,4 +122,22 @@ function FinalizarGrabacion() {
 function SubirGrabacionAGifos() {
     //aparecer pantalla lila con iconos y texto de gifo subido con exito
     // botton 3 lila
+
+
+    fetch("https://upload.giphy.com/v1/gifs", {
+        method: "POST",
+        body: form,
+    })
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(nuevoObjetoRecibido => {
+            console.log(nuevoObjetoRecibido);
+            let gifId = nuevoObjetoRecibido.data.id;  //el id de ese gif subido
+
+            
+        })
+        .catch(error => {
+            console.log("Error! " + error);
+        });
 }
